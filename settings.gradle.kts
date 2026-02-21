a91de4414bf57f6aaaf424af33ae3d189f7282be
narner90 <nateharner01@gmail.com>
2026-02-20 20:49:30 -0700

    Update for intellij 2025.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,5 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "intellij-keymap-xml-exporter"
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
